84ac8b05573cb327dcdc93c0d01bbd862af6e0e6
Jonathan Smith <55775287+JonathanSmith21@users.noreply.github.com>
2020-03-31 13:58:17 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+//Makes the scroll indicator bounce.
+
+$(function() {
+    $('.scroll-down').click (function() {
+      $('html, body').animate({scrollTop: $('section.ok').offset().top }, 'slow');
+      return false;
+    });
+  });
